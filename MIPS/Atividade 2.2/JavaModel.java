889842f8f278d61e3875f6e7d6c77353843197f5
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2023-09-10 22:44:42 -0300

    Iniciando atividade 2.2

---
+++ MIPS/Atividade 2.2/JavaModel.java
@@ -0,0 +1,33 @@
+public class JavaModel{
+
+    //Soma todos os itens do array até o indice indicado
+
+    static int sum(int arr[], int n)
+    {
+        int res = 0;
+        for (int i = 0; i < n; i++)
+            res += arr[i];
+        return res;
+    }
+
+    //Recupera o último impar
+
+    public static int recurImpar(int n)
+    {
+        if (n % 2 == 1)
+            return n;
+        return recurImpar(n - 1);
+    }
+
+
+    public static void main(String[] args)
+    {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int n = 5;
+        int num1 = sum(arr, n);
+        int x = 5;
+        int num2 = recurImpar(x);
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
